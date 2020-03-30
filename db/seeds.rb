00b85d5fc422a0714d53ef5bbb5ceba2468832ed
Patrik Spathon <patrik@spathon.com>
2020-03-30 18:15:31 +0200

    Add some more restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,3 +96,55 @@ ContactChannel.create! name: ContactChannel::TELEPHONE, label: "09 53 09 52 80",
   WeekdayTimeRange.create! day: day, start_in_mins: 10*60, end_in_mins: 22*60, vendor: vendor
 end
 p "Seeded #{vendor.name}"
+
+
+location = Location.create! street: "77 Pl. de la Fruitière",
+                            town: "Les Houches",
+                            latitude: "45.8893722",
+                            longitude: "6.7864525"
+vendor = Vendor.create! name: 'Kitsch Inn',
+                        delivers: true,
+                        location: location
+ContactChannel.create! name: ContactChannel::WEBSITE, label: "www.kitschinn.fr", value: "https://www.kitschinn.fr/", vendor: vendor
+ContactChannel.create! name: ContactChannel::TELEPHONE, label: "04 50 34 41 74", value: "tel:0450344174", vendor: vendor
+%w[Monday Tuesday Wednesday Thursday Friday].each do |day|
+  WeekdayTimeRange.create! day: day, start_in_mins: 16*60+30, end_in_mins: 20*60, vendor: vendor
+end
+%w[Saturday Sunday].each do |day|
+  WeekdayTimeRange.create! day: day, start_in_mins: 13*60, end_in_mins: 20*60, vendor: vendor
+end
+p "Seeded #{vendor.name}"
+
+
+location = Location.create! street: "414 rue Joseph Vallot",
+                            town: "Chamonix",
+                            latitude: "45.9269499",
+                            longitude: "6.8680867"
+vendor = Vendor.create! name: 'Pizzas la Roulotte',
+                        delivers: true,
+                        location: location
+ContactChannel.create! name: ContactChannel::WEBSITE, label: "www.pizzaslaroulotte-chamonix.com", value: "https://www.pizzaslaroulotte-chamonix.com/", vendor: vendor
+ContactChannel.create! name: ContactChannel::TELEPHONE, label: "06 59 56 62 38", value: "tel:0659566238", vendor: vendor
+ContactChannel.create! name: ContactChannel::EMAIL, label: "pizza.laroulotte@gmail.com", value: "mailto:pizza.laroulotte@gmail.com", vendor: vendor
+%w[Monday Tuesday Friday Saturday Sunday].each do |day|
+  WeekdayTimeRange.create! day: day, start_in_mins: 12*60, end_in_mins: 14*60, vendor: vendor
+  WeekdayTimeRange.create! day: day, start_in_mins: 18*60, end_in_mins: 21*60, vendor: vendor
+end
+p "Seeded #{vendor.name}"
+
+
+location = Location.create! street: "Rue de l'Église",
+                            town: "Les Houches",
+                            latitude: "45.8910114",
+                            longitude: "6.7965492"
+vendor = Vendor.create! name: 'Le Délice',
+                        delivers: true,
+                        location: location
+ContactChannel.create! name: ContactChannel::WEBSITE, label: "www.le-delice.info", value: "https://www.le-delice.info/", vendor: vendor
+ContactChannel.create! name: ContactChannel::TELEPHONE, label: "07 85 50 86 52", value: "tel:0785508652", vendor: vendor
+ContactChannel.create! name: ContactChannel::TELEPHONE, label: "04 50 91 52 06", value: "tel:0450915206", vendor: vendor
+ContactChannel.create! name: ContactChannel::EMAIL, label: "ledelicechamonix@gmail.com", value: "mailto:ledelicechamonix@gmail.com", vendor: vendor
+%w[Friday Saturday Sunday].each do |day|
+  WeekdayTimeRange.create! day: day, start_in_mins: 12*60, end_in_mins: 18*60, vendor: vendor
+end
+p "Seeded #{vendor.name}"
