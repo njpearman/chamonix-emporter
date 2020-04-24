c490d0309cf8dddd38be78048bfcdd6903e35127
njp <n.pearman@gmail.com>
2020-04-24 10:47:13 +0200

    Adds crude sort-by-distance feature

--- app/javascript/controllers/sort_controller.js
+++ app/javascript/controllers/sort_controller.js
@@ -1,11 +1,32 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = [ "vendors", "result" ]
+
   connect() {
     console.log("Sort controller connected ... ")
   }
 
   nearest() {
-    console.log("Sorting by nearest")
+    console.log("Sorting by nearest. Found " + this.resultTargets.length + " results")
+    // get an list of results, ordered by distance
+    const orderedResults = this.resultTargets.sort(this.compareNearest)
+    // iterate the ordered list, re-appending them to vendorsTarget
+    orderedResults.forEach(result => {
+      this.vendorsTarget.append(result)
+    })
+  }
+  
+  compareNearest(first, second) {
+    const firstDistance = first.dataset.distance
+    const secondDistance = second.dataset.distance
+
+    if (firstDistance > secondDistance) {
+      return 1
+    } else if (firstDistance < secondDistance) {
+      return -1
+    } else {
+      return 0
+    }
   }
 }
